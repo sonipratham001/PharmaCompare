8cbb40d4165559c370514f2ce6eeda4c2ee6f742
sonipratham001 <sonipratham3008@gmail.com>
2024-12-31 14:02:49 +0530

    editions done

--- PharmaCompareBackend/src/routes/userRoutes.js
+++ PharmaCompareBackend/src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const mongoose = require('mongoose');
 const User = require('../models/User');
 const { jwtSecret } = require('../config');
